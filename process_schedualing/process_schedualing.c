39a1a5254fb249e2271ddd814aab786ea0c438c7
Mark <1624912549@qq.com>
2017-11-25 21:00:25 +0800

    add cfile description

---
+++ process_schedualing/process_schedualing.c
@@ -0,0 +1,21 @@
+/* Project name : Process Schedualing
+ * Author : Mark Kobs
+ * Date : 2017.11.25
+ *
+ * structure design:
+ * 1.PCB
+ * 2.priority_queue based on max_heap(binary heap)
+
+ * functions:
+ * 1(1)initialize
+ * 1(2)lessen
+ * 2(1)initialize by inserting
+ * 2(2)insert
+ * 2(3)delete(remaining)
+ * main(imitating process schedualing in modern computer)
+ */
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+        return 0;
+}
