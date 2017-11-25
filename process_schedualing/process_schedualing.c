5dbb1dd550e9f0d1e22284c981630d531e2cd2a2
Mark <1624912549@qq.com>
2017-11-25 21:36:58 +0800

    add function initialize(PCB,priority_queue)

--- process_schedualing/process_schedualing.c
+++ process_schedualing/process_schedualing.c
@@ -22,14 +22,43 @@ struct PCB{
     int p_id;
     int p_requireTime;
     int p_priority;
-    char state;//E:end,R:ready
+    char p_state;//E:end,R:ready
 };
 
 struct max_heap{
-        int heap_capacity;
+        int capacity;
         int size;
         PCB *pcb_array;
 };
+PCB initialize_pcb(PCB* const pcb,int id,int requireTime,int priority,char state){
+    //PCB* pcb=malloc(sizeof(struct PCB));
+    pcb->p_id=id;
+    pcb->p_requireTime=requireTime;
+    pcb->p_priority=priority;
+    pcb->p_state=state;
+    return *pcb;
+}
+priority_queue initialize_queue(int element_length){
+        priority_queue H;
+        if(element_length<=0){
+                printf("Error,length must be over 0!\n");
+        }
+        H=malloc(sizeof(struct max_heap));
+        if(H==NULL){
+                printf("out of space!\n");
+        }
+        H->pcb_array=malloc(sizeof(struct PCB)*(element_length+1));//the first space for storing empty data.
+        if(H->pcb_array==NULL){
+                printf("out of space!\n");
+        }
+        H->capacity=element_length;
+        H->size=0;
+        H->pcb_array[0]=initialize_pcb(&H->pcb_array[0],0,0,0,' ');
+        printf("%d\n",H->pcb_array[0].p_requireTime);
+}
 int main(){
+        //PCB *p=initialize_pcb(0,0,0,' ');
+       // printf("%d",p->p_id);
+       priority_queue p=initialize_queue(5);
         return 0;
 }
