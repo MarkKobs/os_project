3c3c51d0ff93ec0e4a17c83ba1070d155c383ce7
Mark <1624912549@qq.com>
2017-11-29 23:16:33 +0800

    design the data structure:page list and operation sequence

--- virtual_memory/virtual_memory.c
+++ virtual_memory/virtual_memory.c
@@ -12,10 +12,72 @@
 
 #include<stdlib.h>
 #include<stdio.h>
-typedef page_table *pageTable;
-struct page_table{
+#define OPERATION 12
+#define PAGES 7
+#define INIT_SIZE 4
+typedef struct page *Page;
+typedef struct page_table *PageTable;
+typedef struct instruction_sequence *InstructionSequence;
+int main(int argc,char **argv);
+struct page{
         int page_number;
         int flag;//flag=1 in MM, flag=0 not in MM
         int block_number;
         int position_in_disk;
+
+};
+struct page_table{
+        int capacity;
+        int size;//the size:the number of page in main memory.
+        /*meanwhile,subindex: size-1*/
+        Page page_list;
+};
+struct instruction_sequence{
+        char operation_type;
+        /*operation type includes : + * r w - m */
+        int page_number;
+        int unit_number;
+};
+void setInstruction(InstructionSequence is,char o_t,int p_n,int u_n){
+        is->operation_type=o_t;
+        is->page_number=p_n;
+        is->unit_number=u_n;
+}
+InstructionSequence init_instruction(){
+        InstructionSequence instructionSequence=(InstructionSequence)malloc(OPERATION*sizeof(struct instruction_sequence));
+        if(instructionSequence==NULL){
+                printf("out of space!\n");
+        }
+        /*initialize 12 instruction sequence*/
+        setInstruction(instructionSequence,'+',0,70);
+        setInstruction(instructionSequence+1,'+',1,50);
+        setInstruction(instructionSequence+2,'x',2,15);
+        setInstruction(instructionSequence+3,'w',3,21);
+        setInstruction(instructionSequence+4,'r',0,56);
+        setInstruction(instructionSequence+5,'-',6,40);
+        setInstruction(instructionSequence+6,'m',4,53);
+        setInstruction(instructionSequence+7,'+',5,23);
+        setInstruction(instructionSequence+8,'w',1,37);
+        setInstruction(instructionSequence+9,'r',2,78);
+        setInstruction(instructionSequence+10,'+',4,1);
+        setInstruction(instructionSequence+11,'w',6,84);
+        return instructionSequence;
+
+}
+PageTable init_page_table(){
+        PageTable pt=(PageTable)malloc(sizeof(struct page_table));
+        if(pt==NULL){
+                printf("out of space! \n");
+        }
+        pt->capacity=PAGES;
+        pt->size=INIT_SIZE;
+        pt->page_list=malloc(pt->capacity*sizeof(struct page));//default page list has 7 pages
+}
+
+void print_instruction(InstructionSequence is){
+        printf("%c %d %d\n",is->operation_type,is->page_number,is->unit_number);
+}
+int main(int argc,char **argv){
+        InstructionSequence iSequence=init_instruction();
+        return 0;
 }
