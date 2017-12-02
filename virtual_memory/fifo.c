8eb21f5bcd9cb235f38ebb2c157b0622bb87246b
Mark <1624912549@qq.com>
2017-12-02 14:10:30 +0800

    create a queue for storing the page table

--- virtual_memory/fifo.c
+++ virtual_memory/fifo.c
@@ -7,7 +7,9 @@
  * 其中：标志位1表示对应页已经装入内存，0表示该页未装入内存
  * 主存块号：用来标志装入内存的页所占的块号
  * 在磁盘上的位置：表示页在磁盘上的位置
- *
+ * 
+ * 在实现FIFO的时候务必会用到队列知识，这里用了循环队列的处理方法
+ * 但是在循环队列的基础上，需要用到遍历数组的算法，算法复杂度为O(n)
  * */
 
 #include<stdlib.h>
@@ -18,8 +20,10 @@
 typedef struct page *Page;
 typedef struct page_table *PageTable;
 typedef struct instruction_sequence *InstructionSequence;
+typedef struct queue *Queue;
 int main(int argc,char **argv);
 void print_page(Page page);
+int isEmpty(Queue q);
 struct page{
         int page_number;
         int flag;//flag=1 in MM, flag=0 not in MM
@@ -40,6 +44,80 @@ struct instruction_sequence{
         int page_number;
         int unit_number;
 };
+struct queue{
+        int capacity;
+        int size;
+        int k;
+        int rear;
+        int *array;
+};
+Queue createQueue(int length){
+      Queue q=malloc(sizeof(struct queue));
+      q->capacity=length;
+      q->array=malloc(length*sizeof(int));
+      q->size=0;
+      q->k=0;
+      q->rear=-1;
+      for(int i=0;i<4;i++){
+              q->array[i]=i;
+              q->size++;
+              q->rear++;
+      }
+      return q;
+}
+void print_queue(Queue q){
+        printf("queue capacity:%d,size:%d,k:%d,rear:%d\n",q->capacity,q->size,q->k,q->rear);
+        if(isEmpty(q)){
+                printf("the queue is empty!\n");
+        }
+        else{
+                printf("the queue here:");
+                if(q->k<=q->rear){
+                        for(int i=q->k;i<=q->rear;i++){
+                                printf("%d ",q->array[i]);
+                        }
+                }
+                else{//k>rear
+                        for(int i=q->k;i<q->size;i++){
+                                printf("%d ",q->array[q->size+q->k-q->capacity]);
+                        } 
+                       printf("\n"); 
+                }
+        }
+}
+static int succ(int value,Queue q){//进位
+        if(++value==q->capacity){
+                value=0;
+        }
+        return value;
+}
+int isEmpty(Queue q){
+        return q->size==0;
+}
+int isFull(Queue q){
+        return q->size==q->capacity; 
+}
+int pageIsInArray(int page_number,int *array,int array_length){
+        for(int i=0;i<array_length;i++){
+                if(page_number==array[i]){
+                        return 1;
+                }
+        }
+        return 0;
+}
+void Enqueue(int x,Queue q){//队列中存放的实际上是页表，x就是要插入的页表
+        if(isFull(q)){//满了，就要调出，再将指令中的页号x插入调出的位置
+                printf("%d -----> %d\n",q->array[q->k],x);//原先是array
+                q->k=x;
+                q->k++;
+                
+        }
+        else{
+                q->size++;
+                q->rear=succ(q->rear,q);
+                q->array[q->rear]=x;
+        }
+}
 void setInstruction(InstructionSequence is,char o_t,int p_n,int u_n){
         is->operation_type=o_t;
         is->page_number=p_n;
@@ -118,7 +196,7 @@ int position_transformation(PageTable pt,InstructionSequence is){
 
         return absolute;
 }
-int isFull(PageTable pt){
+int isFullPT(PageTable pt){
         int full=(pt->size==pt->capacity)?1:0;
         return full;
 }
@@ -130,9 +208,14 @@ void setFlagToOne(PageTable pt,int page_number){
         pt->page_list[page_number].flag=1;
         
 }
+void process_interruption(){//处理中断程序
+//TODO
+} 
 int main(int argc,char **argv){
         InstructionSequence seq=init_instruction();//an array actually
         PageTable pt=init_page_table();
+        Queue queue=createQueue(7);
+        print_queue(queue);
         /*实现模拟FIFO执行流程*/
         for(int i=0;i<OPERATION;i++){
                 int page_number=((InstructionSequence)&seq[i])->page_number;
@@ -150,7 +233,7 @@ int main(int argc,char **argv){
                 }
                 else{
                         /*缺页中断处理*/
-                                 
+                        process_interruption();                   
                         /*输出缺页号*/
                         printf("* %d\n",page_number);
                 }
